3cea1127c616c063734fbf57f173759d13edc43c
brent <brenton.j.andrews@gmail.com>
2022-01-06 10:58:54 -0700

    Basic styling / layout almost done, started work on add book popup form

--- script.js
+++ script.js
@@ -1,40 +1,64 @@
 let total_books = document.getElementById("test");
 let total_read = document.getElementById("total_read");
+let library_contents = document.getElementById("library_contents");
+let add_book_btn = document.getElementById("add_btn");
+let delete_book_btn = document.getElementById("delete_btn");
 
 let myLibrary = [];
 
 
 // Book constructor!
-function book(title, author, pages) {
+function book(title, author, pages, publish_date) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.publish_date = publish_date;
 
    this.info = function() {
-        console.log(`${title} by ${author}, ${pages} pages`);
         return `${title} by ${author}, ${pages} pages`;
     }
 }
 
+// add_book_btn functionality.
+add_book_btn.addEventListener("click", () => console.log("click!"));
+
+// Add book object to myLibrary array.
 function addBookToLibrary(bookObject) {
     myLibrary.push(bookObject);
 }
 
+// Use myLibrary info to fill webpage with library content.
 function fillLibrary(myLibrary) {
     for(let i = 0; i < myLibrary.length; i++) {
-        console.log(i);
+        // Create the display card.
+        const card = document.createElement("div");
+        card.className = "card";
+
+        // Create all children elements.
+        const title = document.createElement("p");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const publish_date = document.createElement("p");
+
+        // Add Text Content to each text element.
+        title.textContent = "Title: " + myLibrary[i].title;
+        author.textContent = "Author: " + myLibrary[i].author;
+        pages.textContent = "Pages: " + myLibrary[i].pages;
+        publish_date.textContent = "Publish date: " + myLibrary[i].publish_date;
+
+        // Append new elements to the card.
+        card.append(title, author, pages, publish_date);
+        library_contents.appendChild(card);
     }
 }
 
-const theHobbit = new book("The Hobbit", "J.R.R Tolkien", 295);
-const harryPotter = new book("Harry Potter", "Rowling", 295);
-const algorithmsFourth = new book("Algorithms", "Sedgewick", 600);
+// Add books to library for testing / visualization -> delete later!
+const theHobbit = new book("The Hobbit", "J.R.R Tolkien", 295, "11/12/1930");
+const harryPotter2 = new book("Harry Potter and the Chamber of Secrets", "J.K Rowling", 295, "01/25/1999");
 addBookToLibrary(theHobbit);
-addBookToLibrary(harryPotter);
-addBookToLibrary(algorithmsFourth);
+addBookToLibrary(harryPotter2);
+
 
 fillLibrary(myLibrary);
-console.log(`Books in Library: ${myLibrary.length}`);
-console.log(`Books read: 0`);
 total_books.textContent = `Books in Library: ${myLibrary.length}`;
 total_read.textContent = `Books read: 0`;
